cf59ac38e73f19f55032ac4d318870b8bfb2eb9a
Albert Wang <13521713510@163.com>
2025-03-03 00:02:37 +0800

    Revert "fix: Update .gitignore, remove redundant files"
    This reverts commit f24c8785bd9614485108b1e60960f3d57bcf072a.

--- Debug/subdir_vars.mk
+++ Debug/subdir_vars.mk
@@ -2,15 +2,16 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
+SHELL = cmd.exe
+
 # Add inputs and outputs from these tool invocations to the build variables 
 SYSCFG_SRCS += \
 ../gpio_toggle_output.syscfg 
 
 C_SRCS += \
-../battery.c \
 ../gpio_toggle_output.c \
 ./ti_msp_dl_config.c \
-/Applications/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/m0p/startup_system_files/ticlang/startup_mspm0g350x_ticlang.c \
+C:/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/m0p/startup_system_files/ticlang/startup_mspm0g350x_ticlang.c \
 ../remote.c 
 
 GEN_CMDS += \
@@ -22,7 +23,6 @@ GEN_FILES += \
 ./ti_msp_dl_config.c 
 
 C_DEPS += \
-./battery.d \
 ./gpio_toggle_output.d \
 ./ti_msp_dl_config.d \
 ./startup_mspm0g350x_ticlang.d \
@@ -32,7 +32,6 @@ GEN_OPTS += \
 ./device.opt 
 
 OBJS += \
-./battery.o \
 ./gpio_toggle_output.o \
 ./ti_msp_dl_config.o \
 ./startup_mspm0g350x_ticlang.o \
@@ -44,7 +43,6 @@ GEN_MISC_FILES += \
 ./Event.dot 
 
 OBJS__QUOTED += \
-"battery.o" \
 "gpio_toggle_output.o" \
 "ti_msp_dl_config.o" \
 "startup_mspm0g350x_ticlang.o" \
@@ -56,7 +54,6 @@ GEN_MISC_FILES__QUOTED += \
 "Event.dot" 
 
 C_DEPS__QUOTED += \
-"battery.d" \
 "gpio_toggle_output.d" \
 "ti_msp_dl_config.d" \
 "startup_mspm0g350x_ticlang.d" \
@@ -68,10 +65,9 @@ GEN_FILES__QUOTED += \
 "ti_msp_dl_config.c" 
 
 C_SRCS__QUOTED += \
-"../battery.c" \
 "../gpio_toggle_output.c" \
 "./ti_msp_dl_config.c" \
-"/Applications/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/m0p/startup_system_files/ticlang/startup_mspm0g350x_ticlang.c" \
+"C:/ti/mspm0_sdk_2_03_00_07/source/ti/devices/msp/m0p/startup_system_files/ticlang/startup_mspm0g350x_ticlang.c" \
 "../remote.c" 
 
 SYSCFG_SRCS__QUOTED += \
